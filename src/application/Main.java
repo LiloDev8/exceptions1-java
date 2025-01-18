f20f638162bde0dea1c681ff3f8c6b9b8caa9f72
Murylo Anjos <muryloanjos51@gmail.com>
2025-01-18 10:20:20 -0300

    Exercício de Fixação

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,9 @@
 package application;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
 
-import model.entities.Reservation;
-import model.exceptions.DomainException;
+import model.entities.Account;
+import model.exceptions.WithdrawException;
 
 public class Main {
 
@@ -47,50 +44,88 @@ public class Main {
 //    	sc.close();
 //    	System.out.println("*****MeTHod2 Start");
 		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+//		
+//		try {
+//		System.out.println("Room number : ");
+//		int number = sc.nextInt();
+//		
+//		System.out.println("Check-in date (dd/MM/yyyy)");
+//		Date checkIn = sdf.parse(sc.next());
+//		
+//		System.out.println("Check-out date (dd/MM/yyyy)");
+//		Date checkOut = sdf.parse(sc.next());
+//	
+//			Reservation reservation = new Reservation(number, checkIn, checkOut);
+//			System.out.println("Reservation: " + reservation.toString());
+//			
+//			System.out.println();
+//			System.out.println("Enter data to update the reservation:");
+//			
+//			System.out.println("Check-in date (dd/MM/yyyy)");
+//			checkIn = sdf.parse(sc.next());
+//			
+//			System.out.println("Check-out date (dd/MM/yyyy)");
+//			checkOut = sdf.parse(sc.next());
+//			
+//				
+//		reservation.UpdateDates(checkIn, checkOut);
+//		
+//	
+//	    System.out.println("Reservation: " + reservation.toString());
+//	
+//		}catch(ParseException e){
+//			System.out.println("Invalid date format");
+//			
+//		}catch(DomainException e) {
+//			System.out.println("Error in reservation " + e.getMessage());
+//			
+//		}catch(RuntimeException e) {
+//			System.out.println("Unexpected error");
+//		}
+//		
+//	
+//	sc.close();
+		
+		
 		Scanner sc = new Scanner(System.in);
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
 		try {
-		System.out.println("Room number : ");
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
 		int number = sc.nextInt();
 		
-		System.out.println("Check-in date (dd/MM/yyyy)");
-		Date checkIn = sdf.parse(sc.next());
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
 		
-		System.out.println("Check-out date (dd/MM/yyyy)");
-		Date checkOut = sdf.parse(sc.next());
-	
-			Reservation reservation = new Reservation(number, checkIn, checkOut);
-			System.out.println("Reservation: " + reservation.toString());
-			
-			System.out.println();
-			System.out.println("Enter data to update the reservation:");
-			
-			System.out.println("Check-in date (dd/MM/yyyy)");
-			checkIn = sdf.parse(sc.next());
-			
-			System.out.println("Check-out date (dd/MM/yyyy)");
-			checkOut = sdf.parse(sc.next());
-			
-				
-		reservation.UpdateDates(checkIn, checkOut);
+		System.out.print("InitiaL balance: ");
+		Double balance = sc.nextDouble();
 		
-	
-	    System.out.println("Reservation: " + reservation.toString());
-	
-		}catch(ParseException e){
-			System.out.println("Invalid date format");
-			
-		}catch(DomainException e) {
-			System.out.println("Error in reservation " + e.getMessage());
+		System.out.print("Withdraw limit: ");
+		Double withdrawLimit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+	    
+		System.out.println();
+		
+		System.out.print("Enter amount for withdraw: ");
+		Double amount = sc.nextDouble();
+		
+		acc.withdraw(amount);
+		System.out.println(acc.toString());
+		
+		}catch(WithdrawException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
 			
 		}catch(RuntimeException e) {
 			System.out.println("Unexpected error");
 		}
 		
-	
-	sc.close();
+		
+		sc.close();
 	}
 
 }
